d66d270a49408b05cf4c5c44e354f86959ca2f38
~venir <luca.venir@wde.it>
2025-08-26 15:41:42 +0200

    exporting the current sp version as "legacy"

---
+++ lib/legacy.dart
@@ -0,0 +1,5 @@
+/// Legacy shared preferences APIs.
+/// See https://pub.dev/packages/shared_preferences#sharedpreferences-vs-sharedpreferencesasync-vs-sharedpreferenceswithcache
+library;
+
+export 'src/legacy.dart';
--- lib/riverpod_shared_preferences.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
-
-export 'src/riverpod_shared_preferences_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
